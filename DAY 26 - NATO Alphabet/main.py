c75891dc0ed273e43db9be77db479a977c32d831
Conraduouou <louiserafaellalu@gmail.com>
2021-06-21 16:59:55 +0800

    Modified Day 26 main.py to include error handling.

--- DAY 26 - NATO Alphabet/main.py
+++ DAY 26 - NATO Alphabet/main.py
@@ -1,11 +1,14 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
 data = pandas.read_csv("DAY 26 - NATO Alphabet/nato_phonetic_alphabet.csv")
 phonetics = {row["letter"]:row["code"] for (index, row) in data.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-
-coded_word = [phonetics[letter] for letter in input("Enter a word: ").upper()]
-
-print(coded_word)
+while True:
+    user_input = input("Enter a word: ").upper()
+    try:
+        coded_word = [phonetics[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in the alphabet please.")
+    else:
+        print(coded_word)
+        break
