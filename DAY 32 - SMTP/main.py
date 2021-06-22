a99ae01ffe045f6dcd8ccca0b6c79a12f12f51d5
Conraduouou <louiserafaellalu@gmail.com>
2021-06-22 18:19:52 +0800

    Added whole DAY 32. Easy peasy!

---
+++ DAY 32 - SMTP/main.py
@@ -0,0 +1,54 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+import pandas
+import smtplib
+import random
+import datetime as dt
+
+# function that returns bool if month and day matches today
+def is_match(Datetime, p_details):
+    if Datetime.month == p_details["month"]:
+        return Datetime.day == p_details["day"]
+    
+    return False
+
+# birthdays csv acquired through pandas
+birthdays = pandas.read_csv("DAY 32 - SMTP/birthdays.csv")
+birthdays_list = birthdays.to_dict(orient="records")
+
+# letters to be accessed with respect to experiment-python folder
+letters_list = [f"DAY 32 - SMTP/letter_templates/letter_{number}.txt" for number in range(1, 4)]
+
+# datetime
+now = dt.datetime.now()
+
+# email and password
+my_email = "nakko927saved@gmail.com"
+password = "conradoislife23"
+
+# loop through each person in birthday list
+for person in birthdays_list:
+    if is_match(now, person):
+        path = random.choice(letters_list)
+
+        with open(path) as letter:
+            contents = letter.read()
+            contents = contents.replace("[NAME]", person["name"])
+            contents = contents.replace("Angela", "Nakko <3")
+
+        with smtplib.SMTP("smtp.gmail.com") as connection:
+            connection.starttls()
+            connection.login(my_email, password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs="test.2712@yahoo.com",
+                msg=f"Subject: Happy Birthday, flover!\n\n{contents}"
+            )
